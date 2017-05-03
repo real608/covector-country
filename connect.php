014843da381a2386d45357d3ca656da01df251c2
Database <truehumanity22@gmail.com>
2017-05-03 17:04:03 -0400

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?
+/*
+SANS 3.0 Connect.php
+!!! Only edit if you have issues with your database configuration or you have migrated your database !!!
+
+Author: Database
+Created: 5/3/17
+(c) 2017 SANS Development and the site owner(s)
+*/
+
+$db = mysqli_connect("host","user","password","sansdb") or exit("There was an error accessing the main database. Please reference to the SANS help guide with this error code: 1");
+?>
